db91438d1bafa6cfd8cbdba0010143b1bd4c2ef9
RuteCoelho <rutecoelho86@gmail.com>
2021-04-21 22:50:35 +0100

    new_push

---
+++ frontend/src/screens/AgendaScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CheckoutSteps from '../components/CheckoutSteps';
+
+export default function AgendaScreen(){
+    return (
+        <div>
+            <CheckoutSteps >step1 step2 step3 step4 </CheckoutSteps>
+            <div>
+                <h1>Set Delivery Agenda</h1>
+            </div>
+
+        </div>
+    )
+}
